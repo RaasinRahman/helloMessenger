36eb9455e954dedbf01cc40438755d5912f90b34
Raasin Amin Rahman <77879314+RaasinRahman@users.noreply.github.com>
2023-12-02 22:08:58 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-// App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+  // App.js
+  import React from 'react';
+  import { NavigationContainer } from '@react-navigation/native';
+  import { createStackNavigator } from '@react-navigation/stack';
+  import ProfileScreen from './ProfileScreen';
+  import HomePage from './HomePage'; // make sure to create this file
+  import ChatScreen from './ChatScreen';
+  import LogIn from './LogIn'; // Import LogIn screen
+  const Stack = createStackNavigator();
 
-import HomePage from './HomePage'; 
-import ChatScreen from './ChatScreen';
+  function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="Login" component={LogIn} /> 
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+  export default App;
